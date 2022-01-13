b4e75806c471dfb02a062f1b287bf0c49e9cfcfa
Jesse Stricker <me@jessestricker.de>
2022-01-13 15:08:11 +0100

    Change test require function to check macro

--- src/fastfmt/fastfmt.test.cpp
+++ src/fastfmt/fastfmt.test.cpp
@@ -8,77 +8,77 @@ using namespace std::string_view_literals;
 template <class T>
 using lim = std::numeric_limits<T>;
 
-template <class T>
-void require_fmt(const T& value, std::string_view expected) {
-  auto out = ff::output{};
-  out << value;
-  REQUIRE(out.str() == expected);
-}
+#define FASTFMT_CHECK(value__, expected__) \
+  do {                                     \
+    auto out = ff::output{};               \
+    out << (value__);                      \
+    CHECK(out.str() == (expected__));      \
+  } while (false)
 
 TEST_CASE("formatting char") {
-  require_fmt('a', "a");
-  require_fmt(' ', " ");
-  require_fmt('\n', "\n");
+  FASTFMT_CHECK('a', "a");
+  FASTFMT_CHECK(' ', " ");
+  FASTFMT_CHECK('\n', "\n");
 }
 
 TEST_CASE("formatting c-string") {
-  require_fmt("", "");
-  require_fmt(" ", " ");
-  require_fmt("abc", "abc");
+  FASTFMT_CHECK("", "");
+  FASTFMT_CHECK(" ", " ");
+  FASTFMT_CHECK("abc", "abc");
 }
 
 TEST_CASE("formatting string_view") {
-  require_fmt(""sv, "");
-  require_fmt(" "sv, " ");
-  require_fmt("abc"sv, "abc");
+  FASTFMT_CHECK(""sv, "");
+  FASTFMT_CHECK(" "sv, " ");
+  FASTFMT_CHECK("abc"sv, "abc");
 }
 
 TEST_CASE("formatting bool") {
-  require_fmt(true, "true");
-  require_fmt(false, "false");
+  FASTFMT_CHECK(true, "true");
+  FASTFMT_CHECK(false, "false");
 }
 
 TEST_CASE("formatting unsigned integer, base 10") {
-  require_fmt(lim<std::uint8_t>::min(), "0");
-  require_fmt(lim<std::uint8_t>::max(), "255");
-  require_fmt(lim<std::uint16_t>::min(), "0");
-  require_fmt(lim<std::uint16_t>::max(), "65535");
-  require_fmt(lim<std::uint32_t>::min(), "0");
-  require_fmt(lim<std::uint32_t>::max(), "4294967295");
-  require_fmt(lim<std::uint64_t>::min(), "0");
-  require_fmt(lim<std::uint64_t>::max(), "18446744073709551615");
+  FASTFMT_CHECK(lim<std::uint8_t>::min(), "0");
+  FASTFMT_CHECK(lim<std::uint8_t>::max(), "255");
+  FASTFMT_CHECK(lim<std::uint16_t>::min(), "0");
+  FASTFMT_CHECK(lim<std::uint16_t>::max(), "65535");
+  FASTFMT_CHECK(lim<std::uint32_t>::min(), "0");
+  FASTFMT_CHECK(lim<std::uint32_t>::max(), "4294967295");
+  FASTFMT_CHECK(lim<std::uint64_t>::min(), "0");
+  FASTFMT_CHECK(lim<std::uint64_t>::max(), "18446744073709551615");
 }
 
 TEST_CASE("formatting signed integer, base 10") {
-  require_fmt(lim<std::int8_t>::min(), "-128");
-  require_fmt(lim<std::int8_t>::max(), "127");
-  require_fmt(lim<std::int16_t>::min(), "-32768");
-  require_fmt(lim<std::int16_t>::max(), "32767");
-  require_fmt(lim<std::int32_t>::min(), "-2147483648");
-  require_fmt(lim<std::int32_t>::max(), "2147483647");
-  require_fmt(lim<std::int64_t>::min(), "-9223372036854775808");
-  require_fmt(lim<std::int64_t>::max(), "9223372036854775807");
+  FASTFMT_CHECK(lim<std::int8_t>::min(), "-128");
+  FASTFMT_CHECK(lim<std::int8_t>::max(), "127");
+  FASTFMT_CHECK(lim<std::int16_t>::min(), "-32768");
+  FASTFMT_CHECK(lim<std::int16_t>::max(), "32767");
+  FASTFMT_CHECK(lim<std::int32_t>::min(), "-2147483648");
+  FASTFMT_CHECK(lim<std::int32_t>::max(), "2147483647");
+  FASTFMT_CHECK(lim<std::int64_t>::min(), "-9223372036854775808");
+  FASTFMT_CHECK(lim<std::int64_t>::max(), "9223372036854775807");
 }
 
 TEST_CASE("formatting integer, base 2") {
-  require_fmt(ff::bin(lim<std::uint8_t>::min()), "0");
-  require_fmt(ff::bin(lim<std::uint8_t>::max()), "11111111");
-  require_fmt(ff::bin(lim<std::int8_t>::min()), "-10000000");
-  require_fmt(ff::bin(lim<std::int8_t>::max()), "1111111");
+  FASTFMT_CHECK(ff::bin(lim<std::uint8_t>::min()), "0");
+  FASTFMT_CHECK(ff::bin(lim<std::uint8_t>::max()), "11111111");
+  FASTFMT_CHECK(ff::bin(lim<std::int8_t>::min()), "-10000000");
+  FASTFMT_CHECK(ff::bin(lim<std::int8_t>::max()), "1111111");
 }
 
 TEST_CASE("formatting integer, base 8") {
-  require_fmt(ff::oct(lim<std::uint8_t>::min()), "0");
-  require_fmt(ff::oct(lim<std::uint8_t>::max()), "377");
-  require_fmt(ff::oct(lim<std::int8_t>::min()), "-200");
-  require_fmt(ff::oct(lim<std::int8_t>::max()), "177");
+  FASTFMT_CHECK(ff::oct(lim<std::uint8_t>::min()), "0");
+  FASTFMT_CHECK(ff::oct(lim<std::uint8_t>::max()), "377");
+  FASTFMT_CHECK(ff::oct(lim<std::int8_t>::min()), "-200");
+  FASTFMT_CHECK(ff::oct(lim<std::int8_t>::max()), "177");
 }
 
 TEST_CASE("formatting integer, base 16") {
-  require_fmt(ff::hex(lim<std::uint8_t>::min()), "0");
-  require_fmt(ff::hex(lim<std::uint8_t>::max()), "ff");
-  require_fmt(ff::hex(lim<std::int8_t>::min()), "-80");
-  require_fmt(ff::hex(lim<std::int8_t>::max()), "7f");
+  FASTFMT_CHECK(ff::hex(lim<std::uint8_t>::min()), "0");
+  FASTFMT_CHECK(ff::hex(lim<std::uint8_t>::max()), "ff");
+  FASTFMT_CHECK(ff::hex(lim<std::int8_t>::min()), "-80");
+  FASTFMT_CHECK(ff::hex(lim<std::int8_t>::max()), "7f");
 }
 
 TEST_CASE("formatting multiple arguments") {
@@ -102,6 +102,6 @@ namespace {
 }
 
 TEST_CASE("formatting custom type") {
-  require_fmt(person{"Alice", 42}, "Alice (age 42)");
-  require_fmt(person{"Bob", 49}, "Bob (age 49)");
+  FASTFMT_CHECK((person{"Alice", 42}), "Alice (age 42)");
+  FASTFMT_CHECK((person{"Bob", 49}), "Bob (age 49)");
 }
